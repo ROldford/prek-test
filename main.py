d689f3f3c4c59af22da2ba235516e82624e52fa3
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2026-02-09 15:48:09 -0800

    feat: Main script added
    Acts as main endpoint for program

---
+++ main.py
@@ -0,0 +1,21 @@
+from loguru import logger
+
+from src.do_thing import do_thing
+from src.models import TestModel
+from src.polars_test import main as polars_main
+
+
+def main():
+    result = do_thing(1.5, 2.5, "The result is:")
+    logger.info(result)
+
+    test_instance = TestModel(id_number=1, name="Test", value=3.14)
+    logger.info(test_instance)
+    logger.info(test_instance.name)
+    logger.info(test_instance.model_dump_json())
+
+    polars_main()
+
+
+if __name__ == "__main__":
+    main()
