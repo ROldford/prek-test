4828eab1834f817111da35421a6269930fca185a
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2026-02-09 15:08:34 -0800

    fix(loguru): Now using loguru instead of print

--- src/do_thing.py
+++ src/do_thing.py
@@ -1,3 +1,6 @@
+from loguru import logger
+
+
 def do_thing(x: float, y: float, z: str) -> str:
     xy = x + y
     return f"{z} {xy}"
@@ -5,4 +8,4 @@ def do_thing(x: float, y: float, z: str) -> str:
 
 if __name__ == "__main__":
     result = do_thing(1.5, 2.5, "The result is:")
-    print(result)
+    logger.info(result)
